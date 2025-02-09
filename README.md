8ef19932e5f07d5bc832bd135a24846e3fdfeb08
noxar_ad <71936957+noxarad@users.noreply.github.com>
2025-02-09 08:48:50 +0300

    Finished custom behaviour
    There is a bug where the border stays the same width with increasing number of pixels, look into that later.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+As in TOP curriculum, etch-a-sketch is finished, although there is a bug, I don't really wanna fix it~
--- default.js
+++ default.js
@@ -1,17 +1,31 @@
 const grid = document.querySelector("#grid")
 grid.style.height = `${window.innerHeight}px`
 grid.style.width = `${window.innerHeight}px`
-for (let i = 0; i < 16; i++){
-    for (let j = 0; j < 16; j++){
-        let pixel = document.createElement("div")
-        pixel.setAttribute("class","pixel")
-        pixel.style.width = `${window.innerHeight / 16 - 6}px`
-        pixel.style.height = `${window.innerHeight / 16 - 6}px`
-        grid.appendChild(pixel)
+function gridGeneration(rows = 16) {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < rows; j++) {
+            let pixel = document.createElement("div")
+            pixel.setAttribute("class", "pixel")
+            pixel.style.width = `${window.innerHeight / rows - 6}px`
+            pixel.style.height = `${window.innerHeight / rows - 6}px`
+            grid.appendChild(pixel)
+        }
+    }
+    grid.addEventListener("mouseover", (e) => {
+        if (e.target.className === "pixel") {
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)} ${Math.floor(Math.random() * 256)} ${Math.floor(Math.random() * 256)})`
+        }
+    })
+}
+function customGeneration(){
+    let inputRow = prompt("Please enter height:", "16")
+    if (inputRow != null){
+        parseInt(inputRow)
+    }
+    if (inputRow <= 100 && inputRow > 0){
+       grid.innerHTML = ''
+        row = inputRow
+        gridGeneration(inputRow)
     }
 }
-grid.addEventListener("mouseover", (e) => {
-    if (e.target.className === "pixel"){
-        e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)} ${Math.floor(Math.random() * 256)} ${Math.floor(Math.random() * 256)})`
-    }
-})
+window.onload = gridGeneration()
